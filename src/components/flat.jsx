26904e4f2b5a799a4dd6f870b160ce5b58ebee3e
guillermo varela <guillovarela16@gmail.com>
2020-07-15 19:55:55 -0300

    Finish the list of flats

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import flats from '../data/flats.js';
 
 class Flat extends Component {
   render() {
     return(
-      <div className="card" style= {{ backgroundImage: `url(${this.props.imageUrl})` }}>
+      <div className="card" style= {{ backgroundImage: `url('${this.props.url}')` }}>
         <div className="card-category">
-          {this.props.price}
+        {this.props.price}
         </div>
         <div className="card-description">
-          <h2>{flats[0].name}</h2>
+          <h2>{this.props.name}</h2>
         </div>
         <a className="card-link" href="#"></a>
       </div>
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -3,10 +3,16 @@ import Flat from './flat.jsx';
 import flats from '../data/flats.js';
 
 class FlatList extends Component {
+
+  renderFlats = () => {
+    return flats.map((flat, key) => {
+      return <Flat key={flat.lng} name={flat.name} price={flat.price} url={flat.imageUrl} />
+    })
+  }
   render() {
     return(
       <div className="flat-list">
-        <Flat />
+        {this.renderFlats()}
       </div>
 
       )
