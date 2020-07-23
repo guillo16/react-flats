3b684ac05ea7b72a07ad65a8455de01b5b809a8d
guillermo varela <guillovarela16@gmail.com>
2020-07-23 15:42:55 -0300

    Create new files

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,18 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  render() {
+    return(
+        <div>
+          <div className="flat-list">
+            guillo crack
+          </div>
+          <div className="map-container">
+            hola
+          </div>
+        </div>
+      )
+  }
+}
+
+export default App
